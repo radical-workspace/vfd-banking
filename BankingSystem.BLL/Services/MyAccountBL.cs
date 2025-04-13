dbadda70187980f49fdcb9eacee7e4bb2652d9f4
lsa3edii <moohamedaibrahim7@gmail.com>
2025-04-13 15:42:45 +0200

    Fixed Search Method

---
+++ BankingSystem.BLL/Services/MyAccountBL.cs
@@ -0,0 +1,162 @@
+﻿using BankingSystem.BLL.Interfaces;
+using BankingSystem.DAL.Data;
+using BankingSystem.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+namespace BankingSystem.BLL.Services
+{
+    public partial class MyAccountBL : IGenericRepository<Account>, ISearchPaginationRepo<Account>
+    {
+        private readonly BankingSystemContext _context;
+
+        public MyAccountBL(BankingSystemContext context)
+        {
+            _context = context;
+        }
+
+
+        public IEnumerable<Account> GetAllByPagination(string? ID, string? filter, out int totalRecords, out int totalPages, int pageNumber = 1)
+        {
+            int pageSize = 5;
+
+            var query = _context.Accounts
+                .Include(a => a.Customer)
+                    .ThenInclude(c => c.Branch)
+                .Include(a => a.Branch)
+                .Where(a => a.Customer!.Branch.Tellers.Any(t => t.Id == ID));
+
+
+            if (filter != null)
+                query = query.Where(a => a.AccountStatus.ToString() == filter);
+
+
+            totalRecords = query.Count();
+            totalPages = (int)Math.Ceiling(totalRecords / (double)pageSize);
+
+            if (pageNumber < 1) pageNumber = 1;
+            if (totalPages > 0 && pageNumber > totalPages) pageNumber = totalPages;
+
+
+            return query
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+        }
+
+
+        public IEnumerable<Account> GetAll(string? ID, int flag = 1)
+        {
+            if (flag == 1)
+                return _context.Accounts
+                    .Include(a => a.Customer)
+                        .ThenInclude(c => c.Branch)
+                    .Include(a => a.Branch)
+                    .Where(a => a.Customer!.Branch.Tellers.Any(teller => teller.Id == ID))
+                    .ToList();
+
+            else
+                return _context.Accounts
+                    .Include(a => a.Customer)
+                        .ThenInclude(c => c.Branch)
+                    .Include(a => a.Branch)
+                    .Where(a => a.Customer!.Id == ID)
+                    .ToList();
+        }
+
+
+        public IEnumerable<Account> Search(string search, string? tellerID)
+        {
+            if (search == null)
+                return [];
+
+            var query = GetAll(tellerID)
+                .Where(a => a.Number.ToString()
+                    .Contains(ISearchPaginationRepo<Account>.MyRegex().Replace(search.Trim(), " ")));
+
+            if (!query.Any())
+                query = GetAll(tellerID)
+                        .Where(a => (a.Customer?.FirstName + " " + a.Customer?.LastName).ToLower().Trim()
+                            .Contains(ISearchPaginationRepo<Account>.MyRegex().Replace(search.Trim(), " ")));
+
+            return query;
+        }
+
+
+
+        public Account? Get(int id)
+        {
+            return _context.Accounts
+                //.IgnoreQueryFilters()
+                .Include(a => a.Customer)
+                .Include(a => a.Branch)
+                .FirstOrDefault(a => a.Id == id);
+        }
+
+
+        public void Add(Account Entity)
+        {
+            if (Entity != null)
+            {
+                int custAccountCount = _context.Accounts
+                   .Where(a => a.CustomerId == Entity.CustomerId)
+                   .Count();
+
+                if (custAccountCount < 2)
+                {
+                    _context.Accounts.Add(Entity);
+                    _context.SaveChanges();
+                }
+                else
+                    throw new InvalidOperationException("This customer already have 2 accounts, cannot add more than 2.");
+            }
+        }
+
+
+        public void Update(Account Entity)
+        {
+            if (Entity != null)
+            {
+                _context.Accounts.Update(Entity);
+                _context.SaveChanges();
+            }
+        }
+
+
+        public void Delete(Account entity)
+        {
+            if (entity != null)
+            {
+                var account = _context.Accounts.FirstOrDefault(a => a.Id == entity.Id);
+                if (account != null)
+                {
+                    account.IsDeleted = true;
+                    _context.SaveChanges();
+                }
+            }
+        }
+
+
+
+
+        public IEnumerable<Account> GetAllIncluding(params System.Linq.Expressions.Expression<Func<Account, object>>[] includes)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public Account? GetSingleIncluding(System.Linq.Expressions.Expression<Func<Account, bool>> predicate, params System.Linq.Expressions.Expression<Func<Account, object>>[] includes)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
