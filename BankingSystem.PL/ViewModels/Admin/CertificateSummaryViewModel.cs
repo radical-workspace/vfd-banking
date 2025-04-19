542fc5468c2980ce27b129784bed844056428a52
Ofaa85 <youssefroaf94@gmail.com>
2025-04-19 13:02:10 +0200

    AdminCertficate,Transaction,Loans

---
+++ BankingSystem.PL/ViewModels/Admin/CertificateSummaryViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BankingSystem.PL.ViewModels.Admin
+{
+    public class CertificateSummaryViewModel
+    {
+        public string Type { get; set; }
+        public int Count { get; set; }
+        public double? Sum { get; set; }
+    }
+
+}
